dfe48a59d977eeb95dcc6f7ce685f35b5d689ec4
GhyselsT <thomghysels@gmail.com>
2023-03-03 14:59:08 +0100

    fixed reservatiesys

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ from Reservatie import *
 from Reservatiesysteem import *
 
 r = Reservatiesysteem()
-
+print()
 r.addGebruiker(1,"sejar","dindar","sejar@10")
-
+print()
 r.addFilm(1,"thematrix",6.1)
-
+print()
 r.addReservatie(1,"TG","12","1","4")
-
+print()
 r.addVertoning(1,20,4,"2 oktober", 1)
-
+print()
 r.addZaal(10,200)
