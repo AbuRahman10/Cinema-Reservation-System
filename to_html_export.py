ff7e99d25723d71ca9ec51177082c5027ce5241b
GhyselsT <thomghysels@gmail.com>
2023-03-07 21:40:14 +0100

    Heb een test html Exporter toegevoegd

---
+++ to_html_export.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+my_dict = {
+    'MOVIE': ['THE Matrix', 'Up', 'Maze Runner'],
+    'ID': [1, 2, 3],
+    'TIJD': [30, 40, 50],
+    'ZAAL': [1, 2, 3]
+}
+my_data = pd.DataFrame(data=my_dict)
+my_data.to_html('C:/Users/thomg/PycharmProjects/SCHOOL/MainTOg/test.html')
